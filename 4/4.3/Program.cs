72b6cb65f2e308b68bcaa66e9af82e6bdec39f64
CHINTAN1923 <64345445+CHINTAN1923@users.noreply.github.com>
2022-02-08 12:07:00 +0530

    Add files via upload

---
+++ 4/4.3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+public class SaleItem
+{
+   string _name;
+   decimal _cost;
+
+   public SaleItem(string name, decimal cost)
+   {
+      _name = name;
+      _cost = cost;
+   }
+
+   public string Name
+   {
+      get => _name;
+      set => _name = value;
+   }
+
+   public decimal Price
+   {
+      get => _cost;
+      set => _cost = value;
+   }
+}
+
+class Program
+{
+   static void Main(string[] args)
+   {
+      var item = new SaleItem("Shoes", 19.95m);
+      Console.WriteLine($"{item.Name}: sells for {item.Price:C2}");
+   }
+}
---
+++ 4/4.5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using method;
+
+class Program
+{
+    static void Main()
+    {
+        var tempRecord = new TempRecord();
+
+
+        tempRecord[3] = 58.3F;
+        tempRecord[5] = 60.1F;
+
+    
+        for (int i = 0; i < 10; i++)
+        {
+            Console.WriteLine($"Element #{i} = {tempRecord[i]}");
+        }
+
+        Console.WriteLine("Press any key to exit.");
+        Console.ReadKey();
+    }
+
+}

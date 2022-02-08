f494f01b9413c020620200a5e124f266271d84d8
CHINTAN1923 <64345445+CHINTAN1923@users.noreply.github.com>
2022-02-08 12:08:24 +0530

    Add files via upload

---
+++ 4/4.6/mthd2.cs
@@ -0,0 +1,26 @@
+using System;
+namespace method2;
+
+
+class DayCollection
+{
+    string[] days = { "Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat" };
+
+
+    public int this[string day] => FindDayIndex(day);
+
+    private int FindDayIndex(string day)
+    {
+        for (int j = 0; j < days.Length; j++)
+        {
+            if (days[j] == day)
+            {
+                return j;
+            }
+        }
+
+        throw new ArgumentOutOfRangeException(
+            nameof(day),
+            $"Day {day} is not supported.\nDay input must be in the form \"Sun\", \"Mon\", etc");
+    }
+}
---
+++ 4/4.7/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using dow;
+
+class Program
+{
+    static void Main()
+    {
+        var week = new DayOfWeekCollection();
+        Console.WriteLine(week[DayOfWeek.Friday]);
+
+        try
+        {
+            Console.WriteLine(week[(DayOfWeek)43]);
+        }
+        catch (ArgumentOutOfRangeException e)
+        {
+            Console.WriteLine($"Not supported input: {e.Message}");
+        }
+    }
+
+}

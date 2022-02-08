5d285b50318b66a46f8a8b0be3c501070ed77732
CHINTAN1923 <64345445+CHINTAN1923@users.noreply.github.com>
2022-02-08 12:03:54 +0530

    Add files via upload

---
+++ 4/4.2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public class Person
+{
+   private string _firstName;
+   private string _lastName;
+
+   public Person(string first, string last)
+   {
+      _firstName = first;
+      _lastName = last;
+   }
+
+   public string Name => $"{_firstName} {_lastName}";
+}
+
+public class Example
+{
+   public static void Main()
+   {
+      var person = new Person("Chintan", "Gamit");
+      Console.WriteLine($"Name: {person.Name}, Time: {DateTime.Now.ToString("HH:mm:ss tt")}");
+   }
+}
